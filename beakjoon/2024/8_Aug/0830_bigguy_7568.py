ab5607086fa710654c7afbf610f8f0092034ecd7
sjlim32 <32sjlim@gmail.com>
2024-09-05 18:59:15 +0900

    24.09.05: 백준 (1sol)

--- beakjoon/2024/8_Aug/0830_bigguy_7568.py
+++ beakjoon/2024/8_Aug/0830_bigguy_7568.py
@@ -1,6 +1,10 @@
 # 두 사람 A 와 B의 덩치가 각각 (x, y), (p, q)라고 할 때 x > p 그리고 y > q 이라면 우리는 A의 덩치가 B의 덩치보다 "더 크다"고 말한다.
 # 두 사람 C와 D의 덩치가 각각 (45, 181), (55, 173)이라면 몸무게는 D가 C보다 더 무겁고, 키는 C가 더 크므로, "덩치"로만 볼 때 C와 D는 누구도 상대방보다 더 크다고 말할 수 없다. ( 공동 순위 )
 
+import sys
+
+input = sys.stdin.readline
+
 lst = []
 ans = []
 
@@ -8,10 +12,17 @@ for i in range(int(input())):
   weight, height = map(int, input().split())
   lst.append([weight, height])
 
-list = sorted(lst, key=lambda x: x[0], reverse=True)
+list = sorted(lst, key=lambda x: x[0], reverse=True)  # 정렬할 필요가 없다...
 
-for man in list:
-  print(sum(man))
+
+for man in lst:
+  rank = 1
+  for comp in lst:
+    if man == comp:
+      continue
+    elif man[0] < comp[0] and man[1] < comp[1]:
+      rank +=1
+  print(rank)
 
 
 
